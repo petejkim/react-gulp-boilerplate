71bac10209e96f348ea191adb545392fc11e2567
Peter Jihoon Kim <raingrove@gmail.com>
2015-08-14 23:07:51 +0000

    recompile template everytime in dev mode

--- app/server.go
+++ app/server.go
@@ -13,8 +13,9 @@ import (
 )
 
 var (
-	indexTmpl   *template.Template
-	indexCached *bytes.Buffer
+	tmpls *template.Template
+
+	indexTmplCached *bytes.Buffer
 
 	appEnv    = os.Getenv("APP_ENV")
 	__dirname = mustString(filepath.Abs(filepath.Dir(os.Args[0])))
@@ -25,14 +26,10 @@ func init() {
 		appEnv = "development"
 	}
 
-	funcMap := template.FuncMap{
-		"appendMD5": func(path string) string {
-			m, _ := fileMd5(filepath.Join(__dirname, path))
-			return fmt.Sprintf("%s?v=%x", path, m)
-		},
+	if appEnv != "development" {
+		// precompile
+		tmpls = template.Must(compileTemplates())
 	}
-
-	indexTmpl = template.Must(template.New("main").Funcs(funcMap).ParseGlob(filepath.Join(__dirname, "templates", "*.html.tmpl")))
 }
 
 func main() {
@@ -62,19 +59,31 @@ func main() {
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	if appEnv == "development" || indexCached == nil {
+	if appEnv == "development" || indexTmplCached == nil {
 		// don't bother locking because that's more costly
 		b := &bytes.Buffer{}
-		if err := indexTmpl.ExecuteTemplate(b, "index.html.tmpl", nil); err != nil {
-			log.Println("Error: Could not compile template:", err)
+
+		if appEnv == "development" {
+			// recompile template
+			var err error
+			tmpls, err = compileTemplates()
+			if err != nil {
+				log.Println("Error: Could not compile template:", err)
+				http.Error(w, "500 internal server error", http.StatusInternalServerError)
+				return
+			}
+		}
+
+		if err := tmpls.ExecuteTemplate(b, "index.html.tmpl", nil); err != nil {
+			log.Println("Error: Could not render template:", err)
 			http.Error(w, "500 internal server error", http.StatusInternalServerError)
 			return
 		}
-		indexCached = b
+		indexTmplCached = b
 	}
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write(indexCached.Bytes())
+	w.Write(indexTmplCached.Bytes())
 }
 
 func proxyAPI(w http.ResponseWriter, r *http.Request) {
@@ -110,4 +119,15 @@ func mustString(s string, err error) string {
 		log.Fatal(err)
 	}
 	return s
+}
+
+func compileTemplates() (*template.Template, error) {
+	funcMap := template.FuncMap{
+		"appendMD5": func(path string) string {
+			m, _ := fileMd5(filepath.Join(__dirname, path))
+			return fmt.Sprintf("%s?v=%x", path, m)
+		},
+	}
+
+	return template.New("main").Funcs(funcMap).ParseGlob(filepath.Join(__dirname, "templates", "*.html.tmpl"))
 }
